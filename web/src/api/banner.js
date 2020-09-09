3e1ce8b6fdbc4d1ff3ec519d54ecc0979e9cf42b
Bupt-Wangfang <1654944902@qq.com>
2020-09-09 22:50:29 +0800

    add home page

---
+++ web/src/api/banner.js
@@ -0,0 +1,9 @@
+import request from '@/http'
+
+export function getBanners() {
+    return request.get('/banner/home', {}).then(({ data }) => data);
+}
+
+export function getBannerDetail (id) {
+    return request.get('/banner/' + id).then(({ data }) => data)
+}
---
+++ web/src/views/home/index.vue
@@ -0,0 +1,45 @@
+<template>
+ <main class="page home">
+     <!--banner-->
+    <Carousel height="100%" class="banner" trigger="click">
+        <CarouselItem :title="item.title" class="banner-item" @click.native="bannerDetail(item)" v-for="item in banners" :key="item.id">
+            <h4 class='title'>{{ item.title }}</h4>
+            <img :src="item.img_url" alt="" class="banner-img">
+        </CarouselItem>
+    </Carousel>
+    <Backtop :visibility-height="100" :bottom="100">
+        <i class="el-icon-arrow-up"></i>
+    </Backtop>
+    <!--文章列表展示-->
+
+ </main>
+</template>
+
+<script>
+import { Carousel, CarouselItem, Backtop} from 'element-ui'
+import { getBanners } from '@/api/banner'
+export default {
+ components:{
+     Carousel,
+     CarouselItem,
+     Backtop
+ },
+ created() {
+     getBanners().then((res) => {
+         this.banners = res.data;
+     });
+     this.loadList();
+ },
+ data(){
+    return {
+        banners: []
+    }
+ },
+ methods:{
+     bannerDetail(banner) {},
+     loadList() {}
+ },
+}
+</script>
+<style scoped>
+</style>
